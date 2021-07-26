7683c7637ece9b2282eaa8bcf9be48ae36d5aaed
Kamruj akash <source.akash@gmail.com>
2021-07-26 14:40:39 +0600

    retuch update

--- js/script.js
+++ js/script.js
@@ -187,3 +187,73 @@ $('.port_slider').slick({
 });
 
 // ----- end
+
+
+
+// ======== Back to top annimate
+
+$("#back_to_top").click(function () {
+    $("html,body").animate({
+        scrollTop: 0,
+    }, )
+})
+
+// ----- end
+
+// ======== Back to top annimate
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 800, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+
+// ----- end
+
+
+
+
+// ======== fixed menu
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 50) {
+        $(".addFix").addClass("fixedMenu")
+    } else {
+        $(".addFix").removeClass("fixedMenu")
+    }
+
+    // if ($(window) > 1000) {
+    //     $("#back_to_top").fadeIn()
+    // } else {
+    //     $("#back_to_top").fadeOut()
+    // }
+})
+
+// ----- end
